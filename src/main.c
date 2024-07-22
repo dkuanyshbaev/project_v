ab573748e7461fbcf72290e2f9875b769aa29722
Denis K. <konsistentsi@gmail.com>
2024-07-22 19:27:08 +0700

    WIP: screens, moving object, exit confirmation

--- src/main.c
+++ src/main.c
@@ -1,26 +1,73 @@
 #include "raylib.h"
-#include <stdint.h>
 #include <stdlib.h>
 
+typedef enum Screen { LOGO = 0, PLAY, INFO } Screen;
+
 int main() {
+	// Initialization
     const int screenWidth = 1440;
     const int screenHeight = 900;
+    Screen currentScreen = LOGO;
+    Vector2 objectPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+
+    // Disable KEY_ESCAPE to close window, X-button still works
+    SetExitKey(KEY_NULL);
+    // Flag to request window to exit
+    bool exitWindowRequested = false;
+    // Flag to set window to exit
+    bool exitWindow = false;
 
     Texture2D sprite;
     // Sound sound;
-    Music music;
+    // Music music;
 
     InitWindow(screenWidth, screenHeight, "Project V");
     InitAudioDevice();
     SetTargetFPS(60);
-    sprite = LoadTexture("res/texture.png");
-    // sound = LoadSound("file_name.ogg");
-    music = LoadMusicStream("res/music.mp3");
-    PlayMusicStream(music);
+    sprite = LoadTexture("res/img/texture.png");
+    // sound = LoadSound("res/snd/sound.ogg");
+    // music = LoadMusicStream("res/mus/music.mp3");
+    //PlayMusicStream(music);
 
-    while (!WindowShouldClose()) {
+	// Main loop
+    //while (!WindowShouldClose()) {
+    while (!exitWindow) {
         // Update
-        UpdateMusicStream(music);
+    	if (WindowShouldClose() || IsKeyPressed(KEY_ESCAPE)) exitWindowRequested = true;
+
+    	if (exitWindowRequested) {
+    		// A request for close window has been issued, we can save data before closing
+    		// or just show a message asking for confirmation
+    		if (IsKeyPressed(KEY_Y)) exitWindow = true;
+    		else if (IsKeyPressed(KEY_N)) exitWindowRequested = false;
+    	}
+
+        switch(currentScreen) {
+        	case LOGO:{
+        		if (IsKeyPressed(KEY_ENTER)) {
+        			currentScreen = PLAY;
+        		}
+        	} break;
+        	case PLAY:{
+        		//if (IsKeyPressed(KEY_ENTER)) {
+        			//currentScreen = INFO;
+        		//}
+        	} break;
+        	case INFO:{
+        		if (IsKeyPressed(KEY_ENTER)) {
+        			currentScreen = PLAY;
+        		}
+        	} break;
+        	default: break;
+        }
+
+        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_L)) objectPosition.x += 20.0f;
+        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_H)) objectPosition.x -= 20.0f;
+        if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_K)) objectPosition.y -= 20.0f;
+        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_J)) objectPosition.y += 20.0f;
+
+        //UpdateMusicStream(music);
+
         if (IsKeyPressed(KEY_ENTER) &&
             (IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT))) {
             int display = GetCurrentMonitor();
@@ -35,18 +82,37 @@ int main() {
 
         if (IsKeyPressed(KEY_SPACE)) {
             // PlaySound(sound);
+    		currentScreen = INFO;
         }
 
         // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawTexture(sprite, 0, 0, RAYWHITE);
-        DrawText("Hello..", 240, 350, 42, DARKGRAY);
+        if (exitWindowRequested) {
+			DrawRectangle(0, 100, screenWidth, 200, BLACK);
+            DrawText("Are you sure you want to exit program? [Y/N]", 40, 180, 30, WHITE);
+        } else DrawText("Try to close the window to get confirmation message!", 120, 200, 20, LIGHTGRAY);
+
+        //DrawTexture(sprite, 0, 0, RAYWHITE);
+        switch(currentScreen) {
+        	case LOGO:{
+        		DrawRectangle(0, 0, screenWidth, screenHeight, RAYWHITE);
+        		DrawText("welcome. press enter", 240, 350, 42, DARKGRAY);
+        	} break;
+        	case PLAY:{
+        		DrawCircleV(objectPosition, 24, BLACK);
+        	} break;
+        	case INFO:{
+        		DrawText("important info. press enter", 240, 350, 42, DARKGRAY);
+        	} break;
+        	default: break;
+        }
         EndDrawing();
     }
 
-    StopMusicStream(music);
-    CloseAudioDevice();
+	// De-Initialization
+    //StopMusicStream(music);
+    //CloseAudioDevice();
     CloseWindow();
 
     return EXIT_SUCCESS;
